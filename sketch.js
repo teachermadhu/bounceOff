50f4061e8f7b55d10351221f34cbb308d806da0b
teachermadhu <64385453+teachermadhu@users.noreply.github.com>
2020-05-14 18:43:19 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,22 +15,16 @@ function setup() {
 function draw() {
 background(0,0,0);  
 
-bounceOff(movingRect,fixedRect);
-drawSprites();
+if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+  && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
+    movingRect.velocityX = movingRect.velocityX * (-1);
+    fixedRect.velocityX = fixedRect.velocityX * (-1);
 }
 
-
-
-function bounceOff(object1,object2){
-  if (object1.x - object2.x < object2.width/2 + object1.width/2
-    && object2.x - object1.x < object2.width/2 + object1.width/2) {
-      object1.velocityX = object1.velocityX * (-1);
-      object2.velocityX = object2.velocityX * (-1);
-  }
-  
-   if (object1.y - object2.y < object2.height/2 + object1.height/2
-    && object2.y - object1.y < object2.height/2 + object1.height/2){
-      object1.velocityY = object1.velocityY * (-1);
-      object2.velocityY = object2.velocityY * (-1);
-  }
+ if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+  && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    movingRect.velocityY = movingRect.velocityY * (-1);
+    fixedRect.velocityY = fixedRect.velocityY * (-1);
+}
+drawSprites();
 }
